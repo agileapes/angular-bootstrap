3b201c408d73f3112aba11afba56de7daaea6913
Mohammad Milad Naseri <naseri@caspian.com>
2014-03-09 11:35:04 +0330

    Added alert directive

---
+++ js/directives/alert.js
@@ -0,0 +1,22 @@
+(function (toolkit) {
+    toolkit.register("alert", function (registry) {
+        registry.alert = new toolkit.classes.Directive("1.0", "alert", function () {
+            return {
+                restrict: "E",
+                replace: true,
+                transclude: true,
+                templateUrl: registry.alert.templateUrl,
+                scope: {
+                    type: "@",
+                    dismissible: "=?"
+                },
+                controller: function ($scope) {
+                    if (typeof $scope.dismissible == "undefined") {
+                        $scope.dismissible = false;
+                    }
+                }
+            };
+        });
+    });
+
+})(BootstrapUI);
