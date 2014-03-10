b303cfac9ac9ea21df8db785e22452598b096f69
Mohammad Milad Naseri <naseri@caspian.com>
2014-03-10 16:42:00 +0330

    Added code annotation Added code support to code annotation tooltip Centralized documentation JavaScript

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,14 @@
 $(document).on("ui.ready", function (e, state) {
     state.bind(angular.module("demoApplication", [], null));
 });
+$(function () {
+    $("pre code").each(function () {
+        hljs.highlightBlock(this);
+    });
+    $("[data-toggle=tooltip]").tooltip();
+    $("pre code .annotation").tooltip({
+        container: "body",
+        placement: "top",
+        html: true
+    });
+});
