abeb41a714c1911dd6d9ef7371c62fd2cede4ca3
Mohammad Milad Naseri <naseri@caspian.com>
2014-03-12 00:53:02 +0330

    Added ImpressBox index page

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,22 @@
 $(document).on("ui.ready", function (e, state) {
-    state.bind(angular.module("demoApplication", [], null));
+    var module = angular.module("demoApplication", [], null);
+    module.directive("impressBox", function () {
+        return {
+            restrict: "E",
+            replace: true,
+            transclude: true,
+            scope: {
+                link: "@",
+                glyph: "@",
+                variation: "@"
+            },
+            template: "<a class='{{variation}} sliding up impress-box' ng-href='{{link}}'>" +
+                "<span class='glyph landmark'><span class='glyphicon glyphicon-{{glyph}}'></span></span>" +
+                "<span class='caption' ng-transclude></span>" +
+                "</a>"
+        };
+    });
+    state.bind(module);
 });
 $(function () {
     $("pre code").each(function () {
