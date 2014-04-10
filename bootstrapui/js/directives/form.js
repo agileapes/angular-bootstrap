23729d60aeb976082a8f6c5f513c85fe8c8b50dd
Mohammad Milad Naseri <naseri@caspian.com>
2014-04-11 03:48:37 +0430

    fix (directives:form): remove obsolete implementation

--- bootstrapui/js/directives/form.js
+++ bootstrapui/js/directives/form.js
@@ -53,14 +53,12 @@
             };
         }));
         bu$directives.register("formInput", bu$directives.instantiate(function () {
-            var deferred = $q.defer();
-            deferred.resolve(directiveLoader('input'));
             return {
-                template: deferred.promise,
+                template: '<div><code>&lt;{{namespace ? namespace + \':\' : \'\'}}form-input type="{{type}}"/&gt;</code><span></span></div>',
                 restrict: "E",
                 replace: true,
                 transclude: false,
-                require: "^(bui)FormContainer",
+                require: "^?bu$FormContainer",
                 scope: {
                     type: "@",
                     label: "@",
@@ -80,17 +78,22 @@
                     placeholder: "",
                     validation: "",
                     state: "normal",
-                    feedback: "",
-                    orientation: "vertical",
-                    labelSize: 3
+                    feedback: ""
                 },
                 link: ["scope", "element", "attrs", "controller", function (scope, element, attrs, controller) {
                     if (!scope.labelSize && controller && controller.$scope && controller.$scope.labelSize) {
                         scope.labelSize = controller.$scope.labelSize;
                     }
+                    if (!scope.labelSize) {
+                        scope.labelSize = 3;
+                    }
                     if (!scope.orientation && controller && controller.$scope && controller.$scope.orientation) {
                         scope.orientation = controller.$scope.orientation;
                     }
+                    if (!scope.orientation) {
+                        scope.orientation = "vertical";
+                    }
+                    scope.namespace = bu$configuration.namespace;
                 }],
                 controller: function ($scope) {
                     if (angular.isDefined($scope.descriptor) && angular.isDefined($scope.descriptor())) {
@@ -108,63 +111,5 @@
                 }
             }
         }));
-        var notFound = "<div class='form-error {{orientation}} {{orientation == \"horizontal\" ? \"col-sm-offset-\" + labelSize + \" col-sm-\" + (12 - labelSize) : \"\"}}'><div class=''>&lt;{{namespace ? namespace + ':' : ''}}form-{{component}} type='{{type}}'/&gt;</div></div>";
-        var directiveLoader = function (type) {
-            return ["$scope", "$element", "$attrs", function ($scope, $element, $attrs) {
-                var deferred = $q.defer();
-                bu$loader.load({
-                    type: "form." + type,
-                    identifier: $scope.type,
-                    pathResolver: function (item) {
-                        return config.directivesBase + "/" + item.identifier + ".js";
-                    }
-                }).then(function (loaded) {
-                    var element = form[type].get(loaded[0].identifier);
-                    if (angular.isFunction(element) || (angular.isArray(element) && element.length > 0 && angular.isFunction(element[element.length - 1]))) {
-                        element = $injector.invoke(element, form, {
-                            scope: $scope,
-                            $scope: $scope,
-                            element: $element,
-                            $element: $element,
-                            attrs: $attrs,
-                            $attrs: $attrs
-                        });
-                    }
-                    if (!/\.[^\.]+$/.test(element.templateUrl)) {
-                        element.templateUrl = bu$configuration.base + "/" + config.templatesBase + "/" + element.templateUrl + ".html";
-                        element.templateUrl = element.templateUrl.replace(/\/{2,}/g, "/");
-                    }
-                    deferred.resolve(element);
-                }, function (reason) {
-                    var fail = function (reason) {
-                        if (config.visualErrors) {
-                            $scope.namespace = bu$configuration.namespace;
-                            $scope.component = type;
-                            deferred.resolve(notFound);
-                        } else {
-                            deferred.reject(reason);
-                        }
-                    };
-                    if (reason.status == 404) {
-                        var element = {
-                            templateUrl: $scope.type
-                        };
-                        if (config.aliases[type][element.templateUrl]) {
-                            element.templateUrl = config.aliases[type][element.templateUrl];
-                        }
-                        element.templateUrl = bu$configuration.base + "/" + config.templatesBase + "/" + element.templateUrl + ".html";
-                        element.templateUrl = element.templateUrl.replace(/\/{2,}/g, "/");
-                        $http.head(element.templateUrl).then(function () {
-                            deferred.resolve(element);
-                        }, function (reason) {
-                            fail(reason);
-                        });
-                    } else {
-                        fail(reason);
-                    }
-                });
-                return deferred.promise;
-            }];
-        };
     }]);
 })(eval("$injector"));
