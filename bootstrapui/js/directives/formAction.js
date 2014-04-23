36a4a294d5c34fc60935fd9f01e8789abc7e54b5
Mohammad Milad Naseri <naseri@caspian.com>
2014-04-23 12:17:53 +0430

    feat (directives/formAction): add form action directive which should host actions for the form

---
+++ bootstrapui/js/directives/formAction.js
@@ -0,0 +1,15 @@
+(function (BootstrapUI) {
+    BootstrapUI.directive('formAction', [
+        {
+            'type': 'directive',
+            identifier: 'formContainer'
+        }
+    ], function () {
+        return {
+            require: "^bu$FormContainer",
+            restrict: "E",
+            templateUrl: "formAction",
+            transclude: true,
+        };
+    });
+})(dependency('BootstrapUI'));
