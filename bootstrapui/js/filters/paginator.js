dd95a8c6e85648aed7d12f45ca3551ec05cb5619
Mohammad Milad Naseri <naseri@caspian.com>
2014-04-24 14:09:38 +0430

    feat (filters/paginator): add paginator filter which enables windowed view over a collection of items

---
+++ bootstrapui/js/filters/paginator.js
@@ -0,0 +1,35 @@
+(function (BootstrapUI) {
+
+    BootstrapUI.filter('paginator', function () {
+        return function (input, pageSize, pageNumber) {
+            if (!angular.isArray(input)) {
+                return input;
+            }
+            var output = [];
+            if (!angular.isNumber(pageNumber)) {
+                pageNumber = 1;
+            }
+            if (!angular.isNumber(pageSize)) {
+                pageSize = input.length;
+            }
+            if (pageSize <= 0) {
+                pageSize = 1;
+            }
+            if (pageSize >= input.length) {
+                return input;
+            }
+            var pages = Math.ceil(input.length / pageSize);
+            if (pageNumber <= 0) {
+                pageNumber = 1;
+            }
+            if (pageNumber >= pages) {
+                pageNumber = pages;
+            }
+            for (var i = (pageNumber - 1) * pageSize; i < Math.min(pageNumber * pageSize, input.length); i++) {
+                output.push(input[i]);
+            }
+            return output;
+        };
+    });
+
+})(dependency('BootstrapUI'));
