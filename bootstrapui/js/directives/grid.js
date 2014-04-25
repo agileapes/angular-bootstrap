5b3a0f1a61277b3f72213f738ae1c222db2f0724
Mohammad Milad Naseri <naseri@caspian.com>
2014-04-25 18:13:10 +0430

    feat (grid): add ability to promise values + use events for data binding and value loading

--- bootstrapui/js/directives/grid.js
+++ bootstrapui/js/directives/grid.js
@@ -33,7 +33,7 @@
                 controls: true,
                 noData: "Nothing to show :-)"
             },
-            controller: ["$scope", "$element", "$attrs", "$transclude", "$filter", "$sce", "$q", "$interpolate", function controller($scope, $element, $attrs, $transclude, $filter, $sce, $q, $interpolate) {
+            controller: ["$scope", "$element", "$attrs", "$transclude", "$filter", "$sce", "$q", "$interpolate", "bu$event", function controller($scope, $element, $attrs, $transclude, $filter, $sce, $q, $interpolate, bu$event) {
                 var helper = {
                     find: function (key) {
                         var deferred = $q.defer();
@@ -50,13 +50,17 @@
                         return {
                             then: function (success, failure) {
                                 deferred.promise.then(function (descriptor) {
-                                    $scope.$apply.postpone($scope, [function () {
-                                        success.call(null, descriptor);
-                                    }]);
+                                    if (success) {
+                                        $scope.$apply.postpone($scope, [function () {
+                                            success.call(null, descriptor);
+                                        }]);
+                                    }
                                 }, function () {
-                                    $scope.$apply.postpone($scope, [function () {
-                                        failure.call();
-                                    }]);
+                                    if (failure) {
+                                        $scope.$apply.postpone($scope, [function () {
+                                            failure.call();
+                                        }]);
+                                    }
                                 });
                             }
                         };
@@ -66,7 +70,20 @@
                     }
                 };
                 var paginator = $filter('paginator');
+                var range = $filter('range');
                 var controller = this;
+                $scope.view = {};
+                $scope.view.pages = function () {
+                    var event = bu$event($scope, {
+                        pages: Math.ceil($scope.ngModel.data.length / $scope.ngModel.view.rows),
+                        set: function (pages) {
+                            this.pages = pages;
+                        }
+                    });
+                    event.$emit('bu.grid.pages');
+                    $scope.pageList = range([], 5, $scope.view.page, 1, $scope.view.currentPages);
+                    return event.$get().pages();
+                };
                 $scope.$watch('ngModel', function (value) {
                     if (angular.isUndefined(value)) {
                         return;
@@ -94,10 +111,12 @@
                         $scope.ngModel.view = {};
                     }
                     if (!angular.isNumber($scope.ngModel.view.rows)) {
-                        $scope.ngModel.view.rows = $scope.rows | 10;
+                        $scope.ngModel.view.rows = angular.isNumber($scope.rows) ? $scope.rows : 10;
                     }
-                    if (!angular.isNumber($scope.ngModel.view.page)) {
-                        $scope.ngModel.view.page = $scope.page | 0;
+                    if (angular.isUndefined($scope.ngModel.view.page)) {
+                        $scope.ngModel.view.page = angular.isNumber($scope.page) ? $scope.page : 1;
+                    } else {
+                        $scope.ngModel.view.page = parseInt($scope.ngModel.view.page);
                     }
                     if (angular.isUndefined($scope.ngModel.view.controls)) {
                         $scope.ngModel.view.controls = true;
@@ -140,45 +159,89 @@
                     if ($scope.error) {
                         return;
                     }
-                    $scope.view = {};
-                    $scope.view.pages = Math.ceil($scope.ngModel.data.length / $scope.ngModel.view.rows);
-                    $scope.view.page = Math.min(Math.max($scope.ngModel.view.page, 1), $scope.view.pages);
+                    $scope.view.currentPages = $scope.view.pages();
+                    $scope.view.page = Math.min(Math.max($scope.ngModel.view.page, 1), $scope.view.currentPages);
                     $scope.update();
                 }, true);
-                $scope.goto = function (target) {
+                $scope.goto = function (disabled, target) {
+                    if (disabled) {
+                        return;
+                    }
                     if (target <= 1) {
                         target = 1;
                     }
-                    if (target >= $scope.view.pages) {
-                        target = $scope.view.pages;
+                    if (target >= $scope.view.currentPages) {
+                        target = $scope.view.currentPages;
                     }
-                    $scope.ngModel.view.page = target;
+                    if (target == $scope.view.page) {
+                        return;
+                    }
+                    var event = bu$event($scope, {
+                        from: $scope.view.page,
+                        to: target,
+                        cancelled: false,
+                        scope: $scope,
+                        element: $element,
+                        attrs: $attrs,
+                        cancel: function () {
+                            this.cancelled = true;
+                        }
+                    });
+                    event.$emit('bu.grid.navigating');
+                    if (!event.$get().cancelled()) {
+                        $scope.ngModel.view.page = target;
+                        event.$emit('bu.grid.navigated');
+                    }
                 };
                 $scope.update = function () {
                     $scope.view.data = [];
+                    var deferred = $q.defer();
                     var view = paginator($scope.ngModel.data, $scope.ngModel.view.rows, $scope.ngModel.view.page);
-                    var finalView = [];
-                    for (var i = 0; i < view.length; i++) {
-                        finalView.push(angular.extend({}, view[i]));
-                        angular.forEach($scope.ngModel.headers, function (header) {
-                            var item = finalView[i];
-                            if (angular.isUndefined(item[header.id])) {
-                                item[header.id] = function ($scope, $element, $attrs, helper) {
-                                    return header.handler.apply(controller, [$scope, $element, $attrs, helper]);
-                                };
-                            }
-                            if (angular.isFunction(item[header.id])) {
-                                item[header.id] = item[header.id].apply(controller, [$scope, $element, $attrs, helper]);
-                            }
-                            var html = item[header.id] === null ? "null" : (item[header.id] === undefined ? "" : item[header.id].toString());
-                            html = $interpolate(html)({
-                                $key: item[$scope.ngModel.key],
-                                $item: item
-                            });
-                            item[header.id] = $sce.trustAsHtml(html);
+                    var event = bu$event($scope, {
+                        data: view,
+                        page: $scope.view.page,
+                        loading: 'Loading ...',
+                        set: function (value) {
+                            this.data = value;
+                        }
+                    });
+                    event.$emit('bu.grid.data');
+                    view = event.$get().data();
+                    $scope.loading = $sce.trustAsHtml(event.$get().loading());
+                    if (angular.isObject(view) && angular.isFunction(view.then)) {
+                        view.then(function (data) {
+                            deferred.resolve(data);
                         });
+                    } else {
+                        deferred.resolve(view);
                     }
-                    $scope.view.data = finalView;
+                    $scope.view.data = [];
+                    deferred.promise.then(function (data) {
+                        data = paginator(data, $scope.ngModel.view.rows, $scope.ngModel.view.page);
+                        $scope.loading = undefined;
+                        var finalView = [];
+                        for (var i = 0; i < data.length; i++) {
+                            finalView.push(angular.extend({}, data[i]));
+                            angular.forEach($scope.ngModel.headers, function (header) {
+                                var item = finalView[i];
+                                if (angular.isUndefined(item[header.id])) {
+                                    item[header.id] = function ($scope, $element, $attrs, helper) {
+                                        return header.handler.apply(controller, [$scope, $element, $attrs, helper]);
+                                    };
+                                }
+                                if (angular.isFunction(item[header.id])) {
+                                    item[header.id] = item[header.id].apply(controller, [$scope, $element, $attrs, helper]);
+                                }
+                                var html = item[header.id] === null ? "null" : (item[header.id] === undefined ? "" : item[header.id].toString());
+                                html = $interpolate(html)({
+                                    $key: item[$scope.ngModel.key],
+                                    $item: item
+                                });
+                                item[header.id] = $sce.trustAsHtml(html);
+                            });
+                        }
+                        $scope.view.data = finalView;
+                    });
                 };
             }]
         };
